382c31d8d2287caf8cc99e9a2ef9688947ace2a8
Jazzyjz <jasminf2202@gmail.com>
2023-07-20 15:39:53 -0400

    "touched up CSS, added function to hide and display forecast in JS"

--- assets/script.js
+++ assets/script.js
@@ -128,6 +128,8 @@ techBtn.addEventListener('click', function () {
 // Jasmin's Variables and Functions
 //BUTTONS
 let NycBtn = document.querySelector('#NYC')
+let BouBtn = document.querySelector('#BOU')
+let DtxBtn = document.querySelector('#DTX')
 
 //NY
 let Name = document.querySelector('#name');
@@ -146,6 +148,40 @@ let Name4 = document.querySelector('#name4');
 let temp4 = document.querySelector('#temp4');
 let foreCast4 = document.querySelector('#Forecast4');
 
+//BOU
+let Name5 = document.querySelector('#name5');
+let temp5 = document.querySelector('#temp5');
+let foreCast5 = document.querySelector('#Forecast5');
+
+let Name6 = document.querySelector('#name6');
+let temp6 = document.querySelector('#temp6');
+let foreCast6 = document.querySelector('#Forecast6');
+
+let Name7 = document.querySelector('#name7');
+let temp7 = document.querySelector('#temp7');
+let foreCast7 = document.querySelector('#Forecast7');
+
+let Name8 = document.querySelector('#name8');
+let temp8 = document.querySelector('#temp8');
+let foreCast8 = document.querySelector('#Forecast8');
+
+//DTX
+let day9 = document.querySelector('#name9');
+let temp9 = document.querySelector('#temp9');
+let foreCast9 = document.querySelector('#Forecast9');
+
+let day10 = document.querySelector('#name10');
+let temp10 = document.querySelector('#temp10');
+let foreCast10 = document.querySelector('#Forecast10');
+
+let Name11 = document.querySelector('#name11');
+let temp11 = document.querySelector('#temp11');
+let foreCast11 = document.querySelector('#Forecast11');
+
+let Name12 = document.querySelector('#name12');
+let temp12 = document.querySelector('#temp12');
+let foreCast12 = document.querySelector('#Forecast12');
+
 // Request Forecast for NY
 async function getForecast() {
     const response = await fetch("https://api.weather.gov/gridpoints/OKX/40,74/forecast?units=us");
@@ -155,7 +191,7 @@ async function getForecast() {
 
 async function callForecast(){
     const foreCast = await getForecast();
-    Name.textContent= foreCast.properties.periods[0].name;
+    Name.textContent= properties.periods[0].name;
     temp.textContent = foreCast.properties.periods[0].temperature;
     foreCast.textContent = foreCast.properties.periods[0].detailedForecast;
 
@@ -174,8 +210,134 @@ async function callForecast(){
 console.log(foreCast)
 }
 
+let forecastDataDisplayed = false;
+async function callForecast() {
+    if (forecastDataDisplayed) {
+        // Clear the data from the HTML elements
+        Name.textContent = "";
+        temp.textContent = "";
+        foreCast.textContent = "";
+        Name2.textContent = "";
+        temp2.textContent = "";
+        foreCast2.textContent = "";
+        Name3.textContent = "";
+        temp3.textContent = "";
+        foreCast3.textContent = "";
+        Name4.textContent = "";
+        temp4.textContent = "";
+        foreCast4.textContent = "";
+        forecastDataDisplayed = false;
+    } else {
+        const foreCast = await getForecast();
+        Name.textContent = foreCast.properties.periods[0].name;
+        temp.textContent = foreCast.properties.periods[0].temperature;
+        foreCast.textContent = foreCast.properties.periods[0].detailedForecast;
+        Name2.textContent = foreCast.properties.periods[1].name;
+        temp2.textContent = foreCast.properties.periods[1].temperature;
+        foreCast2.textContent = foreCast.properties.periods[1].detailedForecast;
+        Name3.textContent = foreCast.properties.periods[3].name;
+        temp3.textContent = foreCast.properties.periods[3].temperature;
+        foreCast3.textContent = foreCast.properties.periods[3].detailedForecast;
+        Name4.textContent = foreCast.properties.periods[4].name;
+        temp4.textContent = foreCast.properties.periods[4].temperature;
+        foreCast4.textContent = foreCast.properties.periods[4].detailedForecast;
+        console.log(foreCast);
+        forecastDataDisplayed = true;
+    }
+}
+
+// Request Forecast for BOU
+async function getForecast2() {
+    const response2 = await fetch("https://api.weather.gov/gridpoints/BOU/40,105/forecast?units=us");
+    const data2 = await response2.json();
+    return data2;
+}
+async function callForecast2(){
+    const foreCast2 = await getForecast2();
+    Name5.textContent = properties.periods[0].name;
+    temp5.textContent = properties.periods[0].temperature;
+    foreCast5.textContent = foreCast.properties.periods[0].detailedForecast;
+
+    Name6.textContent= foreCast.properties.periods[1].name;
+    temp6.textContent = foreCast.properties.periods[1].temperature;
+    foreCast6.textContent = foreCast.properties.periods[1].detailedForecast;
+
+    Name7.textContent= foreCast.properties.periods[3].name;
+    temp7.textContent = foreCast.properties.periods[3].temperature;
+    foreCast7.textContent = foreCast.properties.periods[3].detailedForecast;
+
+    Name8.textContent= foreCast.properties.periods[4].name;
+    temp8.textContent = foreCast.properties.periods[4].temperature;
+    foreCast8.textContent = foreCast.properties.periods[4].detailedForecast;
+
+console.log(foreCast2)
+}
+let forecastDataDisplayed2 = false;
+async function callForecast2() {
+    if (forecastDataDisplayed2) {
+        // Clear the data from the HTML elements
+        Name5.textContent = "";
+        temp5.textContent = "";
+        foreCast5.textContent = "";
+        Name6.textContent = "";
+        temp6.textContent = "";
+        foreCast6.textContent = "";
+        Name7.textContent = "";
+        temp7.textContent = "";
+        foreCast7.textContent = "";
+        Name8.textContent = "";
+        temp8.textContent = "";
+        foreCast8.textContent = "";
+        forecastDataDisplayed2 = false;
+    } else {
+        const foreCast2 = await getForecast2();
+        Name5.textContent = foreCast.properties.periods[0].name;
+        temp5.textContent = foreCast.properties.periods[0].temperature;
+        foreCast5.textContent = foreCast.properties.periods[0].detailedForecast;
+        Name6.textContent = foreCast.properties.periods[1].name;
+        temp6.textContent = foreCast.properties.periods[1].temperature;
+        foreCast6.textContent = foreCast.properties.periods[1].detailedForecast;
+        Name7.textContent = foreCast.properties.periods[3].name;
+        temp7.textContent = foreCast.properties.periods[3].temperature;
+        foreCast7.textContent = foreCast.properties.periods[3].detailedForecast;
+        Name8.textContent = foreCast.properties.periods[4].name;
+        temp8.textContent = foreCast.properties.periods[4].temperature;
+        foreCast8.textContent = foreCast.properties.periods[4].detailedForecast;
+        console.log(foreCast2);
+        forecastDataDisplayed2 = true;
+    }
+}
+
+// Request Forecast for DTX
+async function getForecast3() {
+    const response3 = await fetch("https://api.weather.gov/gridpoints/ALY/32,96/forecast?units=us");
+    const data3 = await response3.json();
+    return data3;
+}
+async function callForecast3(){
+    const foreCast3 = await getForecast3();
+    day9.textContent = foreCast.properties.periods[0].name;
+    temp9.textContent = foreCast.properties.periods[0].temperature;
+    foreCast9.textContent = foreCast.properties.periods[0].detailedForecast;
+
+    day10.textContent= foreCast.properties.periods[1].name;
+    temp10.textContent = foreCast.properties.periods[1].temperature;
+    foreCast10.textContent = foreCast.properties.periods[1].detailedForecast;
+
+    Name11.textContent= foreCast.properties.periods[3].name;
+    temp11.textContent = foreCast.properties.periods[3].temperature;
+    foreCast11.textContent = foreCast.properties.periods[3].detailedForecast;
+
+    Name12.textContent= foreCast.properties.periods[4].name;
+    temp12.textContent = foreCast.properties.periods[4].temperature;
+    foreCast12.textContent = foreCast.properties.periods[4].detailedForecast;
+
+console.log(foreCast3)
+}
 //event listeners
 NycBtn.addEventListener('click', callForecast);
+BouBtn.addEventListener('click', callForecast2);
+DtxBtn.addEventListener('click', callForecast3);
 
 // Kyle's Variable and Functions
 const signUp = e =>
